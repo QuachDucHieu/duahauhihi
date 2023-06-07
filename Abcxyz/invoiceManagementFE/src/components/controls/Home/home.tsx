2edf9293cf74d92eaaf01e7810d2386f29ba3149
Quach Hieu <hieuquach0813@gmail.com>
2023-06-07 08:21:50 -0700

    hieuqd commit

--- Abcxyz/invoiceManagementFE/src/components/controls/Home/home.tsx
+++ Abcxyz/invoiceManagementFE/src/components/controls/Home/home.tsx
@@ -3,7 +3,6 @@ import { Link } from 'react-router-dom';
 const Home = () => {
     return(
         <div className="main">
-            <div className="header">We love Snow</div>
             <div className="content">
                 <Link to="/expenses">
                     <div className="box">
